e37c77c5ef6a4304ea2daf389e9608cb54e56937
Rich Li <rchrdyli@gmail.com>
2026-08-07 23:59:10 +0000

    test: reject missing local map packages

--- app/src/test/kotlin/com/rich/rallypacenotes/map/LocalMapPackageTest.kt
+++ app/src/test/kotlin/com/rich/rallypacenotes/map/LocalMapPackageTest.kt
@@ -2,6 +2,7 @@ package com.rich.rallypacenotes.map
 
 import java.io.File
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertThrows
 import org.junit.Test
 
 class LocalMapPackageTest {
@@ -14,6 +15,16 @@ class LocalMapPackageTest {
             assertEquals("mbtiles://${mbtiles.absolutePath}", packageUnderTest.mapLibreUri)
         } finally {
             mbtiles.delete()
+        }
+    }
+
+    @Test
+    fun rejectsAnImportedPathThatDoesNotExist() {
+        val missing = File.createTempFile("norcal-missing", ".mbtiles")
+        missing.delete()
+
+        assertThrows(IllegalArgumentException::class.java) {
+            LocalMapPackage.from(missing)
         }
     }
 }
