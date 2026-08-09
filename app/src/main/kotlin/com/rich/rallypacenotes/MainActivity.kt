02744a468f76fc57add4c7d37182ce6a3c3185e7
Rich Li <rchrdyli@gmail.com>
2026-08-09 15:14:56 +0000

    fix: compile hosted map activity

--- app/src/main/kotlin/com/rich/rallypacenotes/MainActivity.kt
+++ app/src/main/kotlin/com/rich/rallypacenotes/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.core.content.ContextCompat
